5243cc133da2d15ca952652620ecf877e37ce4a5
khanhtapcode <25a4041555@hvnh.edu.vn>
2025-06-08 16:14:59 +0700

    hic

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,4 +20,7 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->withCommands([
+        \App\Console\Commands\TestEmail::class,
+    ])->create();
