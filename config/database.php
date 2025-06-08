de74029499c6621dfffe6e8f7c74f1e43f28aa61
khanhtapcode <25a4041555@hvnh.edu.vn>
2025-06-08 13:40:59 +0700

    Fix database config

--- config/database.php
+++ config/database.php
@@ -33,12 +33,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => $jawsdb_url['host'] ?? env('DB_HOST', '127.0.0.1'),
-            'port' => $jawsdb_url['port'] ?? env('DB_PORT', '3306'),
-            'database' => ltrim($jawsdb_url['path'] ?? '', '/') ?: env('DB_DATABASE', 'forge'),
-            'username' => $jawsdb_url['user'] ?? env('DB_USERNAME', 'forge'),
-            'password' => $jawsdb_url['pass'] ?? env('DB_PASSWORD', ''),
+            'url' => env('JAWSDB_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
